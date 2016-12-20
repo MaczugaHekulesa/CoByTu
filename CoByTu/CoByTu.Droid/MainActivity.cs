5d885927f2d4c9daea4e8838abb57359d17d931f
Jacek-Laptop <jacek.lynx@gmail.com>
2016-12-20 22:25:28 +0100

    adding logo icon and presentation screen image

--- CoByTu/CoByTu.Droid/MainActivity.cs
+++ CoByTu/CoByTu.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CoByTu.Droid
 {
-    [Activity(Label = "CoByTu", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CoByTu", Icon = "@drawable/logo_icon_android", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
