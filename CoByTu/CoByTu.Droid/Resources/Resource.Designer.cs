dad709bb569b956c2b2fdb98209c4f420354a291
Adam <adam.d.lynx@gmail.com>
2016-12-20 22:31:26 +0100

    adding drawable to project

--- CoByTu/CoByTu.Droid/Resources/Resource.Designer.cs
+++ CoByTu/CoByTu.Droid/Resources/Resource.Designer.cs
@@ -3312,8 +3312,11 @@ namespace CoByTu.Droid
 			// aapt resource value: 0x7f020063
 			public const int mr_ic_settings_light = 2130837603;
 			
+			// aapt resource value: 0x7f020065
+			public const int notification_template_icon_bg = 2130837605;
+			
 			// aapt resource value: 0x7f020064
-			public const int notification_template_icon_bg = 2130837604;
+			public const int presentation_bcg_1 = 2130837604;
 			
 			static Drawable()
 			{
